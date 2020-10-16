2186cb8e7735132a41f6285db1fb9431346e9c3b
jakechv <jakechvatal@gmail.com>
2020-10-17 01:15:25 +0200

    Add yohji quotes

--- src/pages/prose.jsx
+++ src/pages/prose.jsx
@@ -3,10 +3,10 @@ import { ScribbleCircleLink } from "../components/link"
 
 import Quote from "../components/quote"
 
-const yohji = (
+const yohji = (link) => (
   <>
     ― {"   "}
-    <ScribbleCircleLink url="https://google.com">Yohji</ScribbleCircleLink>
+    <ScribbleCircleLink url={link}>Yohji</ScribbleCircleLink>
     {"   "}
     Yamamoto
   </>
@@ -33,7 +33,7 @@ const Prose = () => (
         "Somewhere in the things humans make, I want to see",
         "scars, failure, disorder, distortion.",
       ]}
-      author={yohji}
+      author={yohji("https://www.powells.com/book/yamamoto-yohji-9780847843541")}
     />
     <Quote
       quoteLines={[
@@ -41,7 +41,9 @@ const Prose = () => (
         `Black is lazy and easy - but mysterious.`,
         `But above all black says this: "I don’t bother you - don’t bother me".`,
       ]}
-      author={yohji}
+      author={yohji(
+        "https://www.powells.com/book/yohji-yamamoto-my-dear-bomb-9789055449798"
+      )}
     />
     <Quote
       quoteLines={[
@@ -49,13 +51,13 @@ const Prose = () => (
         "Copy copy copy copy.",
         "At the end of the copy you will find yourself.",
       ]}
-      author={yohji}
+      author={yohji("https://www.powells.com/book/talking-to-myself-9783882438253")}
     />
     <Quote
       quoteLines={[
         "With my eyes turned to the past, I walk backwards into the future.",
       ]}
-      author={yohji}
+      author={yohji("https://www.youtube.com/watch?v=RAoYM6Fcnaw")}
     />
   </div>
 )
