078ede5d5961bd144dba2be2ee0cb4888e48a952
jakechv <jakechvatal@gmail.com>
2020-10-17 11:19:57 +0200

    Remove bad style on initial page Add eslint_d for better errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
-import { render } from "react-dom"
 import React, { Suspense } from "react"
+import { render } from "react-dom"
 import { Canvas } from "react-three-fiber"
 import { useProgress, Html } from "@react-three/drei"
-import Prose from "./pages/prose"
-
-import FaceFilter from "./pages/faceFilter"
-
 import { AppContainer } from "react-hot-loader"
 import {
   HashRouter as Router,
   Switch,
   Route,
-  NavLink,
+  // NavLink,
   Redirect,
 } from "react-router-dom"
 
+import Prose from "./pages/prose"
+import FaceFilter from "./pages/faceFilter"
 import Scene1 from "./pages/Scene1"
 import Scene2 from "./pages/Scene2"
 import Scene3 from "./pages/Scene3"
@@ -69,67 +67,14 @@ function App({ scene = 1 }) {
   )
 }
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
 function Body() {
   return (
     <Router>
       <main>
-        <div className="frame">
-          <div className="frame__title-wrap">
-            <h1 className="frame__title">Experiments | Jacob Chvatal</h1>
-            <p className="frame__tagline">Interactive web experiments</p>
-          </div>
-          <div className="frame__links">
-            <a href="https://tympanus.net/Development/MenuFullGrid/">
-              Previous demo
-            </a>
-            <a href="https://tympanus.net/codrops/?p=51167">Article</a>
-            <a href="https://github.com/emmelleppi/codrops-r3f-mirrors">GitHub</a>
-          </div>
-          <div className="frame__demos">
-            <NavLink
-              to="/panna"
-              activeClassName="frame__demo--current"
-              className="frame__demo"
-            >
-              PANNA
-            </NavLink>
-            <NavLink
-              to="/olga"
-              activeClassName="frame__demo--current"
-              className="frame__demo"
-            >
-              OLGA
-            </NavLink>
-            <NavLink
-              to="/pedro"
-              activeClassName="frame__demo--current"
-              className="frame__demo"
-            >
-              PEDRO
-            </NavLink>
-
-            <NavLink
-              to="/facefilter"
-              activeClassName="frame__demo--current"
-              className="frame__demo"
-            >
-              FACEFILTER
-            </NavLink>
-
-            <NavLink
-              to="/prose"
-              activeClassName="frame__demo--current"
-              className="frame__demo"
-            >
-              PROSE
-            </NavLink>
-          </div>
-        </div>
         <div className="content">
           <Switch>
             <Route exact path="/">
-              <Redirect to="/panna" />
+              <Redirect to="/olga" />
             </Route>
             <Route exact path="/panna">
               <App scene={1} />
